91995bd1d6a40962637a414dc312bd67abc9f3c5
aminyous <amyn.hichri@gmail.com>
2024-02-21 21:18:56 +0100

    first comlm

---
+++ src/main/java/com/creil/demoCreil/DemoCreilApplication.java
@@ -0,0 +1,13 @@
+package com.creil.demoCreil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCreilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCreilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creil/demoCreil/DemoCreilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creil.demoCreil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCreilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
